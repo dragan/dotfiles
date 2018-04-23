357d8145a0bcd76bed800094ae6f9594c8087364
Nicholas Cronquist <nick@ncronquist.com>
2018-04-22 23:59:40 -0700

    Initial Usage Tweaks
    - Remove last of base16 color settings
    - Setup gnutils instead of mac os utils
    - Remove go path since that will be handled by gvm
    - Use standard curl

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -4,8 +4,3 @@ colors
 
 # Enable colored output on Unix based systems
 export CLICOLOR=1
-
-# Base16 Shell
-BASE16_SHELL=$HOME/.config/base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] \
-  && eval "$($BASE16_SHELL/profile_helper.sh)"
