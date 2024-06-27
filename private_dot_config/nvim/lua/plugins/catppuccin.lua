796d5f2d0348d176d4620894f38f0f5dd32944df
Dale Ragan <dale@ragan.io>
2024-06-27 15:21:06 -0700

    refactor(vim): Refactor vim to neovim
      - Using LazyVim as a baseline
      - Using lua for configuration over VimScript
      - Install LazyGit
      - Theme LazyGit with Catppuccin
      - Disable lua rocks until we have a lua toolchain installed
      - LazyVim Baseline Customizations
        - Options
          - Set buffer window bar title to the file name
        - Plugins
          - Color Scheme: Set to Catppuccin
          - TreeSitter: Specify ensure installed list
          - Telescope: Use the file browser extension
          - NeoTree: Show hidden files in tree
          - LSP: Specify ensure installed list
          - Vim-Tmux: Navigate between tmux and vim fluently
          - Dashboard: Set to NeoVim
    
    Signed-off-by: Dale Ragan <dale@ragan.io>

---
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+    -- add catppuccin
+    { "catppuccin/nvim" },
+
+    -- Configure LazyVim to load catppuccin
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
+    },
+}
---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "regex",
+            "vim",
+            "yaml",
+        },
+    },
+}
