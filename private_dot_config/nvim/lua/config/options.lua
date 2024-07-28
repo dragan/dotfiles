aa76a5e85d6c2ce709923d4e9f05c32abae5633f
Dale Ragan <dale@ragan.io>
2024-07-28 15:27:41 -0700

    refactor(neovim): Refactoring neovim configuration
      - Starting from scratch
      - Using LazyVim as a baseline
      - Using lua for configuration over VimScript
      - LazyVim Baseline Customizations:
        - Options:
          - Disable lua rocks until we have a lua toolchain installed
          - Set buffer window bar title to the file name
        - Plugins:
          - Theme: Set to Catppuccin
          - Telescope: Use the file browser extension
          - NeoTree: Show hidden files in tree
          - Vim-Tmux: Navigate between tmux and vim fluently
          - Dashboard: Set to NeoVim
        - Extras:
          - Chezmoi: Enable chezmoi
    
    Signed-off-by: Dale Ragan <dale@ragan.io>

---
+++ private_dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Set buffer window bar title to the file name
+vim.opt.winbar = "%=%m %f"
---
+++ private_dot_config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  -- Add catppuccin
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
