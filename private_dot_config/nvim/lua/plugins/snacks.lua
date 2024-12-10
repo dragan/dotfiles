3ecc2d0c52c9403f9ed66146bfa0443700221511
Dale Ragan <dale@ragan.io>
2024-12-10 13:04:40 -0800

    fix(dashboard): Migrate dashboard via snacks
      - Removed previous dashboard config
      - Updated lazyvim config
    
    Signed-off-by: Dale Ragan <dale@ragan.io>

---
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        {
+          sections = {
+            { section = "header" },
+            { section = "keys", gap = 1, padding = 1 },
+            { section = "startup" },
+          },
+        },
+      },
+    },
+  },
+}
