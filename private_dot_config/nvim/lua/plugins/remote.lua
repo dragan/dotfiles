84801552f8e8efe656c0ac1d408fe803d663bfae
Dale Ragan <dale@ragan.io>
2025-08-22 17:23:23 -0700

    feat(neovim): Customize remote plugin
      - Add alacritty to our PATH
      - Customize client callback to launch new alacritty window
        and create a new tmux session with the neovim client running.
    
    Signed-off-by: Dale Ragan <dale@ragan.io>

--- private_dot_config/nvim/lua/plugins/remote.lua
+++ private_dot_config/nvim/lua/plugins/remote.lua
@@ -7,4 +7,25 @@ return {
     "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
   },
   config = true,
+  opts = {
+    client_callback = function(port, workspace_config)
+      local title = ("'Remote: %s'"):format(workspace_config.host)
+      local neovim_cmd = ("/opt/homebrew/bin/nvim --server localhost:%s --remote-ui"):format(port)
+      local tmux_cmd = ("/opt/homebrew/bin/tmux new-session -s \"%s\" %s \\; split-window -v \\; attach"):format(
+        workspace_config.host,
+        neovim_cmd
+      )
+      local cmd = ("alacritty msg create-window --title %s --command %s"):format(
+        title,
+        tmux_cmd
+      )
+      vim.fn.jobstart(cmd, {
+        detach = true,
+        on_exit = function(job_id, exit_code, event_type)
+          -- This function will be called when the job exits
+          print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+        end,
+      })
+    end,
+  }
 }
